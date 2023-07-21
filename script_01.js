00fe62066ff9954c3caba245a8dc28f8a5b47c09
Peter <schmidt.multimedia@gmail.com>
2023-07-21 12:17:08 +0200

    Objekte 2 - Hierarchie

--- script_01.js
+++ script_01.js
@@ -33,31 +33,52 @@
 
 /***** Objekte 1 Daten/Funktionen *******/
 
-let person = {
-    firstName: "Bashkim",
-    familyName: "Dauti",
-    salary:[120000, 160000],
-    permission: true,
-    sayHello: function(){
-        return "Hallo, " + this.firstName + "!";
-    }
-};
+// let person = {
+//     firstName: "Bashkim",
+//     familyName: "Dauti",
+//     salary:[120000, 160000],
+//     permission: true,
+//     sayHello: function(){
+//         return "Hallo, " + this.firstName + "!";
+//     }
+// };
 
 
-output(person);
-output(person["firstName"]);
-output(person.firstName);
-output(person.familyName);
-output(person.salary[1]);
-output(person.permission);
+// output(person);
+// output(person["firstName"]);
+// output(person.firstName);
+// output(person.familyName);
+// output(person.salary[1]);
+// output(person.permission);
 
-const text =    "Ich bin " + person.firstName + 
-                " und verdiene " + person.salary[1] +
-                " p.a.";
+// const text =    "Ich bin " + person.firstName + 
+//                 " und verdiene " + person.salary[1] +
+//                 " p.a.";
 
-output(text);
+// output(text);
 
-output(person.sayHello());
+// output(person.sayHello());
+
+/***** Objekte 2 - Hierarchie *******/
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest
+// nested object {{{}}}
+
+let baikal = {
+                value: "10m",
+                deep: {
+                        deeper:{
+                                deepest:"Das Licht - auf 1642m!",
+                                temperature: "4 Grad Celsius"
+                        }
+                }
+}
+
+output(baikal);
+output(baikal.value);
+output(baikal.deep.deeper.deepest);
+output(baikal.deep.deeper.temperature);
 
 
 /** Ausgabe */
